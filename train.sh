a720a92dd3b5079963dcda75a07811f2437ae061
Gangwei XU <gwxu@hust.edu.cn>
2025-12-20 12:59:02 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
-# #### pretrain on the Hypersim dataset at 512x512 resolution
-# python main.py --cfg_file ppd/configs/train_pretrain.yaml pl_trainer.devices=8
+#### pretrain on the Hypersim dataset at 512x512 resolution
+python main.py --cfg_file ppd/configs/train_pretrain.yaml pl_trainer.devices=8
 
-#### finetune on five mixed datasets at 1024x768 resolution
-python main.py --cfg_file ppd/configs/train_finetune.yaml pl_trainer.devices=8
+# #### finetune on five mixed datasets at 1024x768 resolution
+# python main.py --cfg_file ppd/configs/train_finetune.yaml pl_trainer.devices=8
